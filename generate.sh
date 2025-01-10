4c8bd9ac044007ba0f34e5fe84e9a6f096620cd7
niksa <niksahn@gmail.com>
2025-01-10 20:47:55 +0300

    init

--- generate.sh
+++ generate.sh
@@ -3,27 +3,12 @@
 set -e
 
 PROTO_SRC=proto
-PROTO_DEST=service
-
-# Создание директории назначения
-mkdir -p $PROTO_DEST
-
-# Переход в директорию назначения
-cd $PROTO_DEST
-
-# Инициализация Go модуля
-go mod init proto
-
-# Возврат в корневую директорию
-cd ..
 
 # Генерация gRPC кода
-protoc --go_out=$PROTO_DEST --go_opt=paths=source_relative \
-       --go-grpc_out=$PROTO_DEST --go-grpc_opt=paths=source_relative $PROTO_SRC/*.proto
+protoc --go_out=. --go-grpc_out=. \
+  --go_opt=paths=source_relative \
+  --go-grpc_opt=paths=source_relative \
+  *.proto
 
 # Загрузка зависимостей в директории service
-cd $PROTO_DEST
 go mod tidy
-
-# Возврат в корневую директорию после выполнения всех команд
-cd ..
