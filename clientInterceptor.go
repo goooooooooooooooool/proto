34cd6b8bb580e296f3c916ffa0f337c7ef53e37e
niksa <niksahn@gmail.com>
2025-01-12 17:22:55 +0300

    init

--- clientInterceptor.go
+++ clientInterceptor.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
-	"log"
+	"log/slog"
 )
 
 // UnaryClientErrorInterceptor - interceptor для unary RPC
-func UnaryClientErrorInterceptor() grpc.UnaryClientInterceptor {
+func UnaryClientErrorInterceptor(
+	log *slog.Logger,
+) grpc.UnaryClientInterceptor {
 	return func(
 		ctx context.Context,
 		method string,
@@ -17,46 +19,11 @@ func UnaryClientErrorInterceptor() grpc.UnaryClientInterceptor {
 		invoker grpc.UnaryInvoker,
 		opts ...grpc.CallOption,
 	) error {
-		// Вызов RPC метода
 		err := invoker(ctx, method, req, reply, cc, opts...)
-
-		// Если вызов вернул ошибку, обработайте её
 		if err != nil {
-			// Логирование ошибки
-			log.Printf("gRPC Client Error in method %s: %v", method, err)
-
-			// Пример: можно обернуть ошибку в пользовательский формат
+			log.Error("gRPC Client Error in method %s: %v", method, err)
 			return status.Errorf(status.Code(err), "Custom error: %v", err)
 		}
-
-		// Если ошибки нет, вернуть nil
 		return nil
-	}
-}
-
-// StreamClientErrorInterceptor - interceptor для stream RPC
-func StreamClientErrorInterceptor() grpc.StreamClientInterceptor {
-	return func(
-		ctx context.Context,
-		desc *grpc.StreamDesc,
-		cc *grpc.ClientConn,
-		method string,
-		streamer grpc.Streamer,
-		opts ...grpc.CallOption,
-	) (grpc.ClientStream, error) {
-		// Вызов stream метода
-		clientStream, err := streamer(ctx, desc, cc, method, opts...)
-
-		// Если stream вызов вернул ошибку, обработайте её
-		if err != nil {
-			// Логирование ошибки
-			log.Printf("gRPC Client Stream Error in method %s: %v", method, err)
-
-			// Пример: можно обернуть ошибку в пользовательский формат
-			return nil, status.Errorf(status.Code(err), "Custom error: %v", err)
-		}
-
-		// Если ошибки нет, вернуть stream
-		return clientStream, nil
 	}
 }
--- serverInceptor.go
+++ serverInceptor.go
@@ -2,59 +2,29 @@ package proto
 
 import (
 	"context"
-	"log"
+	"log/slog"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/status"
 )
 
 // UnaryServerErrorInterceptor - interceptor для unary RPC, перехватывает и обрабатывает ошибки
-func UnaryServerErrorInterceptor() grpc.UnaryServerInterceptor {
+func UnaryServerErrorInterceptor(
+	log *slog.Logger,
+) grpc.UnaryServerInterceptor {
 	return func(
 		ctx context.Context,
 		req interface{},
 		info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler,
 	) (resp interface{}, err error) {
-		// Вызов основного handler (реализации RPC-метода)
 		resp, err = handler(ctx, req)
-
-		// Если метод вернул ошибку, обработайте её
 		if err != nil {
-			// Логирование ошибки
-			log.Printf("gRPC Error in method %s: %v", info.FullMethod, err)
-
+			log.Error("gRPC Error in method %s: %v", info.FullMethod, err)
 			// Здесь можно кастомизировать статус ошибки или отправить её "как есть"
 			// Например, можно оставить только её публичный статус
 			return nil, status.Convert(err).Err()
 		}
-
-		// Возвращаем без изменений, если нет ошибки
 		return resp, nil
-	}
-}
-
-// StreamServerErrorInterceptor - interceptor для stream RPC
-func StreamServerErrorInterceptor() grpc.StreamServerInterceptor {
-	return func(
-		srv interface{},
-		ss grpc.ServerStream,
-		info *grpc.StreamServerInfo,
-		handler grpc.StreamHandler,
-	) error {
-		// Вызов основного stream-обработчика
-		err := handler(srv, ss)
-
-		// Если stream вернул ошибку, обработайте её
-		if err != nil {
-			// Логирование ошибки
-			log.Printf("gRPC Stream Error in method %s: %v", info.FullMethod, err)
-
-			// Возвратим только её публичный статус
-			return status.Convert(err).Err()
-		}
-
-		// Возвратим nil, если ошибок нет
-		return nil
 	}
 }
